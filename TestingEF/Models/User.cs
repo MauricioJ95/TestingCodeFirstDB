52086dbaed0e542d6c752ce7405a173e6f6177c7
Mauricio Reyna <mauriciojreyna@gmail.com>
2019-02-20 07:05:43 -0500

    inital push

---
+++ TestingEF/Models/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TestingEF.Models
+{
+    public class User
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public User()
+        {
+
+        }
+        public User(string Name, string Email, string Password)
+        {
+            this.Name = Name;
+            this.Email = Email;
+            this.Password = Password;
+        }
+    }
+
+    public class DBItemContext : DbContext 
+    {
+        public DbSet<User> Items { get; set; }
+    }
+}
